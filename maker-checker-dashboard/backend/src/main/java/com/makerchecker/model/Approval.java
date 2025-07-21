7eda59659977eb1281ee11470f805eecdc6b27e5
akanksha123v-svg <akanksha123.v@gmail.com>
2025-07-21 10:14:20 +0530

    table changes for product
    table changes for make checker

--- maker-checker-dashboard/backend/src/main/java/com/makerchecker/model/Approval.java
+++ maker-checker-dashboard/backend/src/main/java/com/makerchecker/model/Approval.java
@@ -1,11 +1,9 @@
 package com.makerchecker.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "approval")
 public class Approval {
 
     @Id
