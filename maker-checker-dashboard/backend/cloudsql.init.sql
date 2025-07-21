39ca9592ac21887d046b485c67a8c27ab3dbc470
Supriyapa2ri79 <supriyapa2ri79@gmail.com>
2025-07-21 15:21:49 +0530

    Update cloudsql.init.sql

--- maker-checker-dashboard/backend/cloudsql.init.sql
+++ maker-checker-dashboard/backend/cloudsql.init.sql
@@ -1,25 +1,27 @@
 CREATE TABLE product (
   id SERIAL PRIMARY KEY,
   product_code VARCHAR(50),
+  product_name VARCHAR(50) NOT NULL,
   rate_of_intrest DECIMAL(5,2)
   effective_from_date DATE,
   effective_to_date DATE,
   is_active BOOLEAN,
-  maker_id INTEGER,
-  checker_id INTEGER
+  user_id VARCHAR(50),
+  user_role VARCHAR(50)
 );
 
 CREATE TABLE approval (
   id SERIAL PRIMARY KEY,
   product_id INTEGER REFERENCES product(id),
-  maker_id INTEGER,
-  checker_id INTEGER,
+  user_id VARCHAR(50),
+  user_role VARCHAR(50),
   status VARCHAR(20),
   comments TEXT
 );
 
 CREATE TABLE user(
-  user_sso,
-  user_name,
-  user_role
+  user_id VARCHAR(50) NOT NULL PRIMARY KEY,
+  user_name VARCHAR(50) NOT NULL,
+  user_pwd VARCHAR(50) NOT NULL,
+  user_role VARCHAR(50) NOT NULL
 );
